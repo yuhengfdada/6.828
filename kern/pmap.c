ce968c9c0a34a99db85c006959042f27dc11b644
yuhengfdada <877751954@qq.com>
2021-03-27 01:51:19 +0000

    checkout

--- kern/pmap.c
+++ kern/pmap.c
@@ -583,7 +583,10 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+	// panic("mmio_map_region not implemented");
+	size_t pgsize = ROUNDUP(size, PGSIZE);
+	boot_map_region(kern_pgdir, base, pgsize, pa, PTE_PCD | PTE_PWT | PTE_W);
+	base += pgsize;
 }
 
 static uintptr_t user_mem_check_addr;
